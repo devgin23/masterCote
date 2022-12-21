ea15d90e374464419f2b8c4483bf4e5c024b0238
Gin <devgin23@gmail.com>
2022-12-21 23:19:38 +0900

    # [프로그래머스]다음 큰 숫자 (풀이 완료)

--- Programmers/nextBigNum.py
+++ Programmers/nextBigNum.py
@@ -4,25 +4,53 @@ from collections import deque
 
 def solution(n):
     answer = 0
+    # n을 2진수 str으로 변환
     b = bin(n)[2:]
+    print(b)
+    # que에 2진수str으로 변환해서 풀이
     que = deque(b)
+    tempList = list()
     length = len(que)
-    n1 = que.count('1')
-    n0 = que.count('0')
-    check1 = False
+    # 1의 개수를 세어 이에 따라 분기
+    n1 = que.count('1')    
     if length == n1 :
+        # 1111 -> 10111
+        que.popleft()
+        que.appendleft('0')
         que.appendleft('1')
     elif n1 == 1 :
+        # 1000 -> 10000
         que.append('0')
     else :
-        pass
+        # 1100110 -> 1101001
+        cnt1 = 0
+        flag = True
+        while que:
+            pop = que.pop()
+            if pop == '1':
+                cnt1 += 1
+            tempList.append(pop)
+
+            if pop == '0' and cnt1 > 0:
+                flag = False
+                break
+        tempList.sort(reverse=True)
+        tempQ = deque(tempList)       
+        que.append(tempQ.popleft())
+        # 110 -> 1001
+        if flag: que.append('0')
+        while tempQ:
+            que.append(tempQ.pop())
         
 
 
 
 
     print(que)
+    binStr = ('').join(que)
+    answer = int(binStr,2)
+
     print(answer)
     return answer
 
-solution(17)
+solution(6)
