5a68bb6e1a2c57cd0ad8652a691fb82a83690feb
Gin <76995293+devgin23@users.noreply.github.com>
2022-12-26 10:11:18 +0900

    시간 복잡도 관련 주석 추가

--- Programmers/moreSpicy02.py
+++ Programmers/moreSpicy02.py
@@ -3,7 +3,10 @@ import heapq
 def solution(scoville, K):
     heapq.heapify(scoville)
     answer = 0
-    while min(scoville) < K:
+# scoville이 heap 자료구조로 변경 되었어도 min함수로 최솟값을 찾는 것은 시간복잡도 O(N) 이다.
+# heap 자료구조로 최소값을 찾는 시간 복잡도는 O(logN)이다.
+#     while min(scoville) < K: 
+    while scoville[0] < K:
         if len(scoville) == 1 and scoville[0] < K:
             answer = -1
             break
