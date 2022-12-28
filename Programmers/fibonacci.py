32c7c701196c8d67f34fcb167b893484d802ac83
gin <devgin23@gmail.com>
2022-12-28 10:11:25 +0900

    #[프로그래머스] 피보나치 수 다른풀이 추가 및 주석

--- Programmers/fibonacci.py
+++ Programmers/fibonacci.py
@@ -1,11 +1,29 @@
 #[프로그래머스] 피보나치 수
+import time
 def solution(n):
+    start = time.time()
     answer = 0
     num = [0,1]
     for i in range(2,n+1):
         num.append((num[i-2]+num[i-1])%1234567)
     answer = num[-1]
     print(answer)
+    end = time.time()
+    print(str((end-start)*1000) + ' ms')
     return answer
 
-solution(5)
+# solution02가 더 빠르다. append를 쓰기보다는 배열을 크게 만들어놓고 인덱스로 넣는 것이 더 빠름.
+def solution02(n):
+    start = time.time()
+    answer = 0
+    num = [0]*(n+2)
+    num[0]=0
+    num[1]=1
+    for i in range(2,n+1):
+        num[i] = ((num[i-2]+num[i-1])%1234567)
+    answer = num[n]
+    print(answer)
+    end = time.time()
+    print(str((end-start)*1000) + ' ms')
+    return answer
+solution(1000000)
